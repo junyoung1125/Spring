3464cbc437ac85db24f423f55151a0636be94511
junyoung1125 <wnsdud060451@gmail.com>
2025-03-18 12:23:36 +0900

    ch07

---
+++ ch07/src/main/java/kr/co/ch07/entity/board/User.java
@@ -0,0 +1,24 @@
+package kr.co.ch07.entity.board;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@Entity
+@Table(name="Board_User")
+public class User {
+    @Id
+    private String uid;
+    private String name;
+    private String hp;
+
+    @CreationTimestamp  //mysql NOW()
+    private LocalDateTime regDate;
+}
